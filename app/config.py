48c15410eec9469fd07a7324c180e92cfc2e41e1
Nords Zamora <100557534+nordszamora@users.noreply.github.com>
2024-07-04 05:59:15 -1200

    first version

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-   SQLALCHEMY_DATABASE_URI = 'mysql://<user>:@<host>:<port>/<database>'
+   SQLALCHEMY_DATABASE_URI = 'mysql://<user>:@<host>:<port>/<database>' # Edit
    SECRET_KEY = 's3cr3t'
    SQLALCHEMY_TRACK_MODIFICATIONS = False
